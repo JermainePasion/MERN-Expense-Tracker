942b4b8422b0e30f8ee41807130ed4f4def3a799
Jermaine_Riley Pasion <jmpasion@student.hau.edu.ph>
2025-04-21 21:18:12 +0800

    first commit / frontend template & routes

---
+++ frontend/expense-tracker/src/App.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
+
+import Login from './screens/Auth/Login';
+import SignUp from './screens/Auth/SignUp'; 
+import Home from './screens/DashBoard/Home';
+import Income from './screens/DashBoard/Income';
+import Expense from './screens/DashBoard/Expense';
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path = "/" element = {<Root/>} />
+          <Route path = "/login" exact element = {<Login/>} />
+          <Route path = "/signup" exact element = {<SignUp/>} />
+          <Route path = "/home" exact element = {<Home/>} />
+          <Route path = "/income" exact element = {<Income/>} />
+          <Route path = "/expense" exact element = {<Expense/>} />
+        </Routes>
+      </Router>
+
+    </div>
+  )
+}
+
+export default App;
+
+const Root = () => {
+
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  return isAuthenticated  ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to = "/login" />
+  );
+
+};
